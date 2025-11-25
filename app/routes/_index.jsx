950748381aa12fb602f08cc57256b4db4e598178
jdev12345 <devjyoti598@gmail.com>
2025-11-25 21:45:36 +0530

    redirection to /{path} instead of /

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,7 +1,8 @@
 import { redirect } from "react-router";
 
-export const loader = () => {
-  return redirect("https://www.superergo.in/", 302);
+export const loader = ({ request }) => {
+  const url = new URL(request.url);
+  return redirect(`https://www.superergo.in${url.pathname}${url.search}`, 302);
 };
 
 export default function Index() {
